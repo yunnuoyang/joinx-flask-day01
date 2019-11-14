a0f920879306928002c34e576816ef7fbaf7102b
yunnuoyang <50757618+yunnuoyang@users.noreply.github.com>
2019-11-14 15:15:47 +0800

    db

--- com/joinx/04/MySQLdb.py
+++ com/joinx/04/MySQLdb.py
@@ -43,13 +43,37 @@ import pymysql #首先需要安装好pymysql模块
 # print(cursor.rowcount)
 # db.close()
 #查询语句
+# db = pymysql.connect("localhost", "root", "root", "pythontest", charset='utf8' )
+# cursor=db.cursor()
+# sql="""select * from user"""
+# cursor.execute(sql)
+# results=cursor.fetchall()
+# for query in results:
+#     id=query[0]
+#     username=query[1]
+#     password=query[2]
+#     print(id,username,password)
+# db.close()
+import pymysql
+
+# 打开数据库连接
 db = pymysql.connect("localhost", "root", "root", "pythontest", charset='utf8' )
-cursor=db.cursor()
-sql="""select * from user"""
-cursor.execute(sql)
-results=cursor.fetchall()
-for query in results:
-    id=query[0]
-    username=query[1]
-    password=query[2]
-    print(id,username,password,)
+
+# 使用cursor()方法获取操作游标
+cursor = db.cursor()
+
+# SQL 删除语句
+sql = "DELETE FROM user WHERE id = %s" % (1)
+try:
+   print(sql)
+   # 执行SQL语句
+   cursor.execute(sql)
+   # 提交修改
+   db.commit()
+except:
+   # 发生错误时回滚
+   print("有错误")
+   db.rollback()
+
+# 关闭连接
+db.close()
